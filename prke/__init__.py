6db2478138107cb15b3144f37850d0e6810c2f25
Katy Huff <katyhuff@gmail.com>
2014-12-02 17:20:12 -0800

    update

--- prke/__init__.py
+++ prke/__init__.py
@@ -1,7 +1,7 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 """
-Prke (Perky) is a point reactor kinetics and thermal hydraulics module for 
-coupled, 0-D transient analysis.
+PRKE (Perky) is a point reactor kinetics and thermal hydraulics solver module 
+for coupled, 0-D transient analysis.
 """
 
 
