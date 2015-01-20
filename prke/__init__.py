02243deb4806ee4ea923a141da63e94d058889ec
Katy Huff <katyhuff@gmail.com>
2015-01-20 11:43:15 -0800

    gets rid of official pronunciation

--- prke/__init__.py
+++ prke/__init__.py
@@ -1,6 +1,6 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 """
-PRKE (Perky) is a point reactor kinetics and thermal hydraulics solver module 
+PRKE is a point reactor kinetics and thermal hydraulics solver module 
 for coupled, 0-D transient analysis.
 """
 
