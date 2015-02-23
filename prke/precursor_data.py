2bfbc2d61754c41610cbf0d9de5fa1a67f591225
Katy Huff <katyhuff@gmail.com>
2015-02-23 15:14:09 -0800

    loving python-mode

--- prke/precursor_data.py
+++ prke/precursor_data.py
@@ -1,5 +1,6 @@
 import math
 
+
 class PrecursorData(object):
     def __init__(self, nuc, e, n):
         """initializes the precursor group data for the fissioning nuclide. e
@@ -34,10 +35,12 @@ class PrecursorData(object):
         beta_dict = {}
         beta_dict["u235"] = {}
         beta_dict["pu239"] = {}
-        beta_dict["u235"]["thermal"] = [0.00247, 0.0013845, 0.001222, 0.0026455, 0.000832, 0.000169]
-        beta_dict["u235"]["fast"] = [0.000266, 0.001491, 0.001316, 0.002849, 0.000896, 0.000182]
-        beta_dict["pu239"]["thermal"] = [0.0,0.0,0.0,0.0,0.0,0.0]
-        beta_dict["pu239"]["fast"] = [0.0,0.0,0.0,0.0,0.0,0.0]
+        beta_dict["u235"]["thermal"] = [0.00247, 0.0013845, 0.001222,
+                                        0.0026455, 0.000832, 0.000169]
+        beta_dict["u235"]["fast"] = [0.000266, 0.001491, 0.001316, 0.002849,
+                                     0.000896, 0.000182]
+        beta_dict["pu239"]["thermal"] = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+        beta_dict["pu239"]["fast"] = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
         return beta_dict[nuc][e]
 
     def _get_lambdas(self, nuc, e):
@@ -45,11 +48,12 @@ class PrecursorData(object):
         lambda_dict = {}
         lambda_dict["u235"] = {}
         lambda_dict["pu239"] = {}
-        lambda_dict["u235"]["thermal"] = [math.log(2)/x for x in \
-                [54.51, 21.84, 6.00, 2.23, 0.496, 0.179]]
+        lambda_dict["u235"]["thermal"] = [math.log(2)/x for x in
+                                          [54.51, 21.84, 6.00, 2.23, 0.496,
+                                           0.179]]
         lambda_dict["u235"]["fast"] = [0.0127, 0.0317, 0.155, 0.311, 1.4, 3.87]
-        lambda_dict["pu239"]["thermal"] = [0.0,0.0,0.0,0.0,0.0,0.0]
-        lambda_dict["pu239"]["fast"] = [0.0,0.0,0.0,0.0,0.0,0.0]
+        lambda_dict["pu239"]["thermal"] = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+        lambda_dict["pu239"]["fast"] = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
         return lambda_dict[nuc][e]
 
     def _get_Lambda(self, nuc, e):
@@ -61,4 +65,3 @@ class PrecursorData(object):
         Lambda_dict["pu239"]["thermal"] = 0
         Lambda_dict["pu239"]["fast"] = 0
         return Lambda_dict[nuc][e]
-
