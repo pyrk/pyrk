dc481275124b1a185d86f2e0ffe509d7985f7aac
Katy Huff <katyhuff@gmail.com>
2015-02-23 20:44:28 -0800

    apparently, empty __init__ is better

--- prke/__init__.py
+++ prke/__init__.py
@@ -3,7 +3,3 @@
 PRKE is a point reactor kinetics and thermal hydraulics solver module
 for coupled, 0-D transient analysis.
 """
-
-from prke.thermal_hydraulics import *
-from prke.neutronics import *
-import prke.version
--- prke/utils/__init__.py
+++ prke/utils/__init__.py
@@ -2,9 +2,3 @@
 """
 This subpackage contains utilities for development used by prke.
 """
-from __future__ import (absolute_import, division, print_function,
-                        unicode_literals)
-
-
-from .exceptions import *
-
