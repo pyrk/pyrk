ad9f88c5055d27cf3a81a74cb3d2d5c11eacacc6
Katy Huff <katyhuff@gmail.com>
2015-02-23 16:14:11 -0800

    moves thermal_hydraulics to th

--- prke/tests/test_th.py
+++ prke/tests/test_th.py
@@ -2,21 +2,22 @@ from nose.tools import assert_equal, assert_almost_equal, assert_true, \
     assert_false, assert_raises, assert_is_instance, with_setup
 
 import numpy as np
-import thermal_hydraulics
+import th
 
 
-components = {"fuel":0,"cool":1,"mod":2,"refl":3}
+components = {"fuel": 0, "cool": 1, "mod": 2, "refl": 3}
+
 
 def test_dtempfueldt_returns_numbers():
-    th = thermal_hydraulics.ThermalHydraulics()
+    test_obj = th.ThermalHydraulics()
     p = 1.0000002
-    omegas = np.array([0,0,0])
+    omegas = np.array([0, 0, 0])
     temps = {}
-    i=0
+    i = 0
     for c in components:
         temps[i] = 750.0
-        i+=1
+        i += 1
 
     for c in components:
-        obs = th.dtempdt(c, temps, p, omegas, components)
+        obs = test_obj.dtempdt(c, temps, p, omegas, components)
         assert(obs > 0)
