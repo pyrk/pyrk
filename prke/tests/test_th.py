572159d97e2a9e7c7afb2f8a34f2c40d950d6be0
Katy Huff <katyhuff@gmail.com>
2015-02-28 13:25:09 -0800

    adds thcomponent class

--- prke/tests/test_th.py
+++ prke/tests/test_th.py
@@ -1,22 +1,21 @@
 from nose.tools import assert_equal, assert_almost_equal, assert_true, \
     assert_false, assert_raises, assert_is_instance, with_setup
 
-import numpy as np
-import thermal_hydraulics
+import th
+from inp import sim_info
 
 
-components = {"fuel":0,"cool":1,"mod":2,"refl":3}
 
-def test_dtempfueldt_returns_numbers():
-    th = thermal_hydraulics.ThermalHydraulics()
-    p = 1.0000002
-    omegas = np.array([0,0,0])
-    temps = {}
-    i=0
-    for c in components:
-        temps[i] = 750.0
-        i+=1
-
-    for c in components:
-        obs = th.dtempdt(c, temps, p, omegas, components)
-        assert(obs > 0)
+def test_constructor():
+    name = "testname"
+    vol = 20
+    k = 10
+    rho = 100
+    T0 = 700
+    si = sim_info.SimInfo(t0=0, tf=10, dt=0.1)
+    tester = th.THComponent(name, vol, k, rho, T0, si)
+    assert_equal(tester.name, name)
+    assert_equal(tester.vol, vol)
+    assert_equal(tester.k, k)
+    assert_equal(tester.rho, rho)
+    assert_equal(tester.T0, T0)
---
+++ prke/th.py
@@ -0,0 +1,29 @@
+import numpy as np
+
+
+class THComponent(object):
+    """This class represents a component of the system it has material and
+    geometric properties essential to thermal modeling and heat transfer in
+    support of calculations related to the thermal_hydraulics subblock
+    """
+
+    def __init__(self, name, vol=0, k=0, rho=0, T0=0, si=None):
+        """Initalizes a thermal hydraulic component
+
+        Arguments
+        ---------
+        name : str
+               The name of the component (a handle, like "fuel" or "cool")
+
+        params : THParams object
+                 An object containing the parameters for this function?
+
+        """
+        self.name = name
+        self.vol = vol
+        self.k = k
+        self.rho = rho
+        self.T0 = T0
+        self.T = np.zeros(shape=(1, si.timesteps()), dtype=float)
+        self.T[0] = T0
+        self.sim_info = si
