1ad056a9b179d12b8cd5432af958197d7adb6039
Katy Huff <katyhuff@gmail.com>
2015-02-27 16:07:55 -0800

    adds siminfo tests

---
+++ prke/inp/tests/test_sim_info.py
@@ -0,0 +1,15 @@
+from nose.tools import assert_equal, assert_almost_equal, assert_true, \
+    assert_false, assert_raises, assert_is_instance, with_setup
+
+from prke.inp import sim_info as si
+
+
+def test_init_reasonable_sim():
+    t0 = 0
+    tf = 10
+    dt = 0.1
+    info = si.SimInfo(t0, tf, dt)
+    assert_equal(t0, info.t0)
+    assert_equal(tf, info.tf)
+    assert_equal(t0, info.t0)
+    assert_equal(info.timesteps(),100)
