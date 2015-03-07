67b4f2df6070dc1cb8b2d3b99b7bd658042f1e86
Katy Huff <katyhuff@gmail.com>
2015-03-06 16:39:14 -0800

    adds somewhat trivial decay heat object constructor tests for the non failing cases

--- pyrk/data/tests/test_decay_heat.py
+++ pyrk/data/tests/test_decay_heat.py
@@ -4,21 +4,47 @@ from nose.tools import assert_equal, assert_almost_equal, assert_true, \
 from data import decay_heat
 
 
-
 def test_u235_thermal_constructor():
     n = 11
     nuc = "u235"
     e = "thermal"
     d = decay_heat.DecayData(nuc=nuc, e=e, n=n)
+    assert_equal(d._n, n)
+    assert_equal(d._e, e)
+    assert_equal(d._nuc, nuc)
+    assert_equal(len(d._lambdas), n)
+    assert_equal(d._lambdas, d.lambdas())
+    assert_equal(d._kappas, d.kappas())
 
 
 def test_u235_fast_constructor():
-    assert_false(False)
+    n = 11
+    nuc = "u235"
+    e = "fast"
+    d = decay_heat.DecayData(nuc=nuc, e=e, n=n)
+    assert_equal(d._n, n)
+    assert_equal(d._e, e)
+    assert_equal(d._nuc, nuc)
 
 
 def test_pu239_thermal_constructor():
-    assert_false(False)
+    # TODO This should actually test for a warning to be issued
+    n = 11
+    nuc = "pu239"
+    e = "thermal"
+    d = decay_heat.DecayData(nuc=nuc, e=e, n=n)
+    assert_equal(d._n, n)
+    assert_equal(d._e, e)
+    assert_equal(d._nuc, nuc)
+
 
 
 def test_pu239_fast_constructor():
-    assert_false(False)
+    # TODO This should actually test for a warning to be issued
+    n = 11
+    nuc = "pu239"
+    e = "fast"
+    d = decay_heat.DecayData(nuc=nuc, e=e, n=n)
+    assert_equal(d._n, n)
+    assert_equal(d._e, e)
+    assert_equal(d._nuc, nuc)
