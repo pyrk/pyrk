2f52739ab2b80069f5b00aa7fb9e12726bfc96bf
Katy Huff <katyhuff@gmail.com>
2015-03-06 16:27:58 -0800

    adds u235 thermal decay data test

--- pyrk/data/tests/test_decay_heat.py
+++ pyrk/data/tests/test_decay_heat.py
@@ -4,5 +4,21 @@ from nose.tools import assert_equal, assert_almost_equal, assert_true, \
 from data import decay_heat
 
 
-def test_stuff():
+
+def test_u235_thermal_constructor():
+    n = 11
+    nuc = "u235"
+    e = "thermal"
+    d = decay_heat.DecayData(nuc=nuc, e=e, n=n)
+
+
+def test_u235_fast_constructor():
+    assert_false(False)
+
+
+def test_pu239_thermal_constructor():
+    assert_false(False)
+
+
+def test_pu239_fast_constructor():
     assert_false(False)
