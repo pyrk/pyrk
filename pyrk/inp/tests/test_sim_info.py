f7865eaa48d9081a8c93d697a41e5bb75ba2b7ae
Katy Huff <katyhuff@gmail.com>
2015-03-17 10:46:19 -0400

    units in tests

--- pyrk/inp/tests/test_sim_info.py
+++ pyrk/inp/tests/test_sim_info.py
@@ -3,11 +3,13 @@ from nose.tools import assert_equal, assert_almost_equal, assert_true, \
 
 from pyrk.inp import sim_info as si
 
+from ur import units
+
 
 def test_init_reasonable_sim():
-    t0 = 0
-    tf = 10
-    dt = 0.1
+    t0 = 0*units.seconds
+    tf = 10*units.seconds
+    dt = 0.1*units.seconds
     info = si.SimInfo(t0, tf, dt)
     assert_equal(t0, info.t0)
     assert_equal(tf, info.tf)
