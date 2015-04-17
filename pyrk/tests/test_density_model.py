1b9eb44f23aa031f51bc0977ffca27e43a3693aa
Katy Huff <katyhuff@gmail.com>
2015-04-17 13:28:14 -0700

    fixes timer and density tests to match improvements

--- pyrk/tests/test_density_model.py
+++ pyrk/tests/test_density_model.py
@@ -1,12 +1,13 @@
 from nose.tools import assert_equal, assert_almost_equal, assert_true, \
     assert_false, assert_raises, assert_is_instance, with_setup
 
+from ur import units
 import density_model
 
 
 def test_default_constructor():
     dm = density_model.DensityModel()
-    assert_equal(dm.a, 0)
-    assert_equal(dm.b, 0)
+    assert_equal(dm.a, 0*units.kg/units.meter**3)
+    assert_equal(dm.b, 0*units.kg/units.kelvin/units.meter**3)
     assert_equal(dm.model, 'linear')
-    assert_equal(dm.rho(), dm.b)
+    assert_equal(dm.rho(), dm.b*units.kelvin)
