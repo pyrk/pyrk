620cd576ff600c61fc0880fd60a82a2877f6e22a
Katy Huff <katyhuff@gmail.com>
2015-04-17 15:01:24 -0700

    tests pass

--- pyrk/tests/test_graphite.py
+++ pyrk/tests/test_graphite.py
@@ -2,20 +2,17 @@ from nose.tools import assert_equal, assert_almost_equal, assert_true, \
     assert_false, assert_raises, assert_is_instance, with_setup
 
 import graphite
-from inp import sim_info
 from ur import units
 from timer import Timer
 
 name = "testname"
-vol = 20*units.meter**3
 kappa = 0
 T0 = 700*units.kelvin
 t0 = 0*units.seconds
 tf = 10*units.seconds
 dt = 0.1*units.seconds
 ti = Timer(t0=t0, tf=tf, dt=dt)
-tester = graphite.Graphite(name=name, vol=vol, T0=T0, timer=ti)
-si = sim_info.SimInfo(kappa=kappa, timer=ti)
+tester = graphite.Graphite(name=name)
 
 k_graphite = 0.26*units.watt/(units.meter*units.kelvin)
 cp_graphite = 1650.0*units.joule/(units.kg*units.kelvin)
@@ -24,22 +21,11 @@ rho_const = 1740.*units.kg/(units.meter**3)
 
 def test_constructor():
     assert_equal(tester.name, name)
-    assert_equal(tester.vol, vol)
     assert_equal(tester.k, k_graphite)
     assert_equal(tester.cp, cp_graphite)
-    assert_equal(tester.rho(timestep=0), rho_const)
-    assert_equal(tester.T0, T0)
+    assert_equal(tester.rho(temp=0*units.kelvin), rho_const)
 
 
 def test_temp():
-    assert_equal(tester.temp(0), T0)
-
-
-def test_update_temp():
-    assert_equal(tester.temp(0), T0)
-    T1 = 10*units.kelvin
-    T2 = 20*units.kelvin
-    tester.update_temp(1, T1)
-    assert_equal(tester.temp(1), T1)
-    tester.update_temp(2, T2)
-    assert_equal(tester.temp(2), T2)
+    assert_equal(tester.rho(temp=0*units.kelvin), rho_const)
+    assert_equal(tester.rho(temp=T0), rho_const)
--- pyrk/tests/test_th_component.py
+++ pyrk/tests/test_th_component.py
@@ -1,12 +1,12 @@
 from nose.tools import assert_equal, assert_almost_equal, assert_true, \
     assert_false, assert_raises, assert_is_instance, with_setup
 
-import th_system
 import th_component as th
 from inp import sim_info
-import density_model
 from ur import units
 from timer import Timer
+from material import Material
+import density_model
 
 name = "testname"
 vol = 20*units.meter**3
@@ -15,14 +15,16 @@ cp = 10*units.joule/units.kg/units.kelvin
 dm = density_model.DensityModel(a=0*units.kg/units.meter**3,
                                 b=100*units.kg/units.meter**3,
                                 model='constant')
+mat = Material(k=k, cp=cp, dm=dm)
+
+
 kappa = 0
 T0 = 700*units.kelvin
 t0 = 0*units.seconds
 tf = 10*units.seconds
 dt = 0.1*units.seconds
 ti = Timer(t0=t0, tf=tf, dt=dt)
-tester = th.THComponent(name=name, vol=vol, k=k, cp=cp, dm=dm, T0=T0,
-                        timer=ti)
+tester = th.THComponent(name=name, mat=mat, vol=vol, T0=T0, timer=ti)
 si = sim_info.SimInfo(kappa=kappa, timer=ti)
 
 
