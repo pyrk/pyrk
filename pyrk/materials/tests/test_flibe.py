c139a79832eb0785ebe406eb45de17105cb3b83b
Katy Huff <katyhuff@gmail.com>
2015-07-22 11:22:20 -0700

    fixes imports for flibe and graphite

--- pyrk/materials/tests/test_flibe.py
+++ pyrk/materials/tests/test_flibe.py
@@ -1,7 +1,7 @@
 from nose.tools import assert_equal, assert_almost_equal, assert_true, \
     assert_false, assert_raises, assert_is_instance, with_setup
 
-import flibe
+from materials import flibe
 from ur import units
 
 name = "testname"
--- pyrk/materials/tests/test_graphite.py
+++ pyrk/materials/tests/test_graphite.py
@@ -1,7 +1,7 @@
 from nose.tools import assert_equal, assert_almost_equal, assert_true, \
     assert_false, assert_raises, assert_is_instance, with_setup
 
-import graphite
+from materials import graphite
 from ur import units
 from timer import Timer
 
