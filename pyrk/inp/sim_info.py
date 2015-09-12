3fb55b16c8c6a0a7eab642288d35c777700c004b
Katy Huff <katyhuff@gmail.com>
2015-09-11 20:21:40 -0700

    adds a bunch of docs to the siminfo class

--- pyrk/inp/sim_info.py
+++ pyrk/inp/sim_info.py
@@ -22,6 +22,27 @@ class SimInfo(object):
                  feedback=False,
                  plotdir='images'):
         """This class holds information about a reactor kinetics simulation
+
+        :param timer: the Timer object for the simulation
+        :type timer: Timer
+        :param components: the components making up the reactor
+        :type components: dictionary
+        :param iso: the main fissioning isotope, decides precursor data
+        :type iso: only a few values are supported. see PrecursorData class
+        :param e: spectrum ("fast", "thermal", etc.)
+        :type e: string
+        :param n_precursors: number of delayed precursor groups
+        :type n_precursors: int
+        :param n_decay: number of decay groups
+        :type n_decay: int
+        :param kappa: the value for kappa, a decay heat parameter
+        :type kappa: float
+        :param rho_ext: external reactivity
+        :type rho_ext: a ReactivityInsertion object or None
+        :param feedback: is reactivity feedback present in the simulation
+        :type feedback: bool
+        :param plotdir: the directory where the plots will be placed
+        :type plotdir: string
         """
         self.timer = timer
         self.components = components
@@ -38,11 +59,18 @@ class SimInfo(object):
         self.plotdir = plotdir
 
     def init_rho_ext(self, rho_ext):
+        """Initializes reactivity insertion object for the none case.
+
+        :param rho_ext: external reactivity
+        :type rho_ext: a ReactivityInsertion object or None
+        """
         if rho_ext is None:
             rho_ext = ri.ReactivityInsertion(self.timer)
         return rho_ext
 
     def init_ne(self):
+        """Initializes the neutronics object owned by the siminfo object
+        """
         ne = neutronics.Neutronics(iso=self.iso, e=self.e,
                                    n_precursors=self.n_pg,
                                    n_decay=self.n_dg,
@@ -52,17 +80,25 @@ class SimInfo(object):
         return ne
 
     def n_components(self):
+        """The number of components in the simulation.
+        """
         to_ret = len(self.components)
         return to_ret
 
     def n_entries(self):
+        """The number of entries in the pde to be solved
+        """
         to_ret = 1 + self.n_pg + self.n_dg + len(self.components)
         return int(to_ret)
 
     def add_th_component(self, th_component):
         """Adds a thermal-hydralic component to this simulation.
         It should be fully initialized. A single simulation may have many
-        thermal-hydralic components. They are held in a dictionary."""
+        thermal-hydralic components. They are held in a dictionary.
+
+        :param th_component: the th_component to add to the system
+        :type th_component: THComponent
+        """
 
         if th_component.name in self.components:
             msg = "A component named "
