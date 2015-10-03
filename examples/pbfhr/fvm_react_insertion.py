fixed units problem, all quantities keep units in the package, until some computation in th_system

examples/pbfhr/fvm_react_insertion.py
@@ -36,13 +36,14 @@ alpha_mod = -0.7*units.pcm/units.kelvin
 alpha_shell = 0*units.pcm/units.kelvin
 alpha_cool = 0.23*units.pcm/units.kelvin
 
-#initial temperature
+# initial temperature
 t_mod = (800+273.15)*units.kelvin
 t_fuel = (800+273.15)*units.kelvin
 t_shell = (770+273.15)*units.kelvin
 t_cool = (650+273.15)*units.kelvin
 
 kappa = 0.0
+
 
 def area_sphere(r):
     assert(r >= 0*units.meter)
@@ -111,23 +112,28 @@ nsteps = 5000
 
 k_mod = 17*units.watt/(units.meter*units.kelvin)
 cp_mod = 1650.0*units.joule/(units.kg*units.kelvin)
-rho_mod =DensityModel(a=1740.*units.kg/(units.meter**3), model="constant")
+rho_mod = DensityModel(a=1740.*units.kg/(units.meter**3), model="constant")
 Moderator = Material('mod', k_mod, cp_mod, rho_mod)
 
 k_fuel = 15*units.watt/(units.meter*units.kelvin)
 cp_fuel = 1818.0*units.joule/units.kg/units.kelvin
-rho_fuel=DensityModel(a=2220.0*units.kg/(units.meter**3), model="constant")
-Fuel=Material('fuel', k_fuel, cp_fuel, rho_fuel)
+rho_fuel = DensityModel(a=2220.0*units.kg/(units.meter**3), model="constant")
+Fuel = Material('fuel', k_fuel, cp_fuel, rho_fuel)
 
 k_shell = 17*units.watt/(units.meter*units.kelvin)
 cp_shell = 1650.0*units.joule/(units.kg*units.kelvin)
-rho_shell =DensityModel(a=1740.*units.kg/(units.meter**3), model="constant")
-Shell=Material('shell', k_shell, cp_shell, rho_shell)
+rho_shell = DensityModel(a=1740.*units.kg/(units.meter**3), model="constant")
+Shell = Material('shell', k_shell, cp_shell, rho_shell)
 
 k_cool = 1*units.watt/(units.meter*units.kelvin)
 cp_cool = 2415.78*units.joule/(units.kg*units.kelvin)
-rho_cool = DensityModel(a=2415.6*units.kg/(units.meter**3), b=0.49072*units.kg/(units.meter**3)/units.kelvin, model="linear")
-cool=Material('cool', k_cool, cp_cool, rho_cool)
+rho_cool = DensityModel(a=2415.6 *
+                        units.kg /
+                        (units.meter**3), b=0.49072 *
+                        units.kg /
+                        (units.meter**3) /
+                        units.kelvin, model="linear")
+cool = Material('cool', k_cool, cp_cool, rho_cool)
 
 mod = th.THComponent(name="mod",
                      mat=Moderator,
@@ -136,7 +142,7 @@ mod = th.THComponent(name="mod",
                      alpha_temp=alpha_mod,
                      timer=ti,
                      sph=True,
-                     ri=0.0,
+                     ri=0.0*units.meter,
                      ro=r_mod)
 
 fuel = th.THComponent(name="fuel",
@@ -180,10 +186,20 @@ cool = th.THComponent(name="cool",
 cool.add_convection('pebble', h=h_cool, area=a_pb)
 cool.add_advection('cool', m_flow/n_pebbles, t_inlet, cp=cool.cp)
 
-components=[]
+components = []
 for i in range(0, len(pebble.sub_comp)):
     components.append(pebble.sub_comp[i])
 components.extend([pebble, cool])
 
-uncert=[alpha_cool, alpha_fuel, k_mod, k_fuel, k_shell, cp_mod, cp_fuel, cp_shell, cp_cool, h_cool]
-uncertainty_param=np.array([o.magnitude for o in uncert])
+uncert = [
+    alpha_cool,
+    alpha_fuel,
+    k_mod,
+    k_fuel,
+    k_shell,
+    cp_mod,
+    cp_fuel,
+    cp_shell,
+    cp_cool,
+    h_cool]
+uncertainty_param = np.array([o.magnitude for o in uncert])

pyrk/th_component.py
@@ -53,7 +53,7 @@ class THComponent(object):
         :type ro: float
         """
         self.name = name
-        self.vol = vol.to('meter**3').magnitude
+        self.vol = vol.to('meter**3')
         self.mat = mat
         self.k = mat.k
         self.cp = mat.cp
@@ -74,8 +74,8 @@ class THComponent(object):
         self.prev_t_idx = 0
         self.convBC = {}
         self.sph = sph
-        self.ri = ri.to('meter').magnitude
-        self.ro = ro.to('meter').magnitude
+        self.ri = ri.to('meter')
+        self.ro = ro.to('meter')
 
     def mesh(self, size):
         '''cut a THComponent into a list of smaller components
@@ -85,7 +85,6 @@ class THComponent(object):
         :type size: float with length unit
         :return: list of smaller components
         '''
-        size = size.magnitude
         if not self.sph:
             msg = 'mesh function only implemented for spherical component'
             raise TypeError(msg)
@@ -102,14 +101,14 @@ class THComponent(object):
             alpha_temp = self.alpha_temp/self.vol*vol
             to_ret.append(THComponent(name=self.name+'%d' % i,
                                       mat=self.mat,
-                                      vol=vol*units.meter**3,
+                                      vol=vol,
                                       T0=self.T0,
                                       alpha_temp=alpha_temp,
                                       timer=self.timer,
                                       heatgen=self.heatgen,
                                       power_tot=power_tot,
                                       sph=self.sph,
-                                      ri=ri*units.meter, ro=ro*units.meter))
+                                      ri=ri, ro=ro))
         return to_ret
 
     def temp(self, timestep):
@@ -140,16 +139,8 @@ class THComponent(object):
 
         :param timestep: the timestep at which to query the temperature
         :type timestep: int
-<<<<<<< HEAD
-        :param temp: the new tempterature
-        :type float: float, dimensionless
-||||||| merged common ancestors
-        :param temp: the new tempterature
-        :type float: float, units of kelvin
-=======
         :param temp: the new temperature
         :type float: float, units of kelvin
->>>>>>> testing_merge
         """
         self.T[timestep] = temp
         self.prev_t_idx = timestep
@@ -297,8 +288,8 @@ class THSuperComponent(THComponent):
                              heatgen=False,
                              power_tot=0*units.watt,
                              sph=False,
-                             ri=0,
-                             ro=0)
+                             ri=0*units.meter,
+                             ro=0*units.meter)
         self.sub_comp = sub_comp
         self.T = units.Quantity(np.zeros(shape=(timer.timesteps(),),
                                          dtype=float), 'kelvin')