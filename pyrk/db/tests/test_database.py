2fac6a53f48c2633f55837b5ad4ac6bdf56fbf6f
Katy Huff <katyhuff@gmail.com>
2015-10-09 16:05:18 -0700

    adds test for the constructor

--- pyrk/db/tests/test_database.py
+++ pyrk/db/tests/test_database.py
@@ -1,8 +1,17 @@
 from nose.tools import assert_equal, assert_almost_equal, assert_true, \
     assert_false, assert_raises, assert_is_instance, with_setup
 
-from db import database
+from db import database as d
 
 
-def test_stuff():
-    assert_false(False)
+def test_default_constructor():
+    a = d.Database()
+    assert_equal(a.mode, 'a')
+    assert_equal(a.title, 'PyRKDatabase')
+    import os.path
+    assert_true(os.path.isfile(a.filepath))
+
+
+def test_custom_constructor():
+    a = d.Database(filepath='testfile.h5')
+    assert_equal(a.filepath, 'testfile.h5')
