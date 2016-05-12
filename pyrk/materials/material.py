13fcb322f02d1a75fcbee5cc2c75e670b79dea6a
Xin Wang <imwangxin@gmail.com>
2016-05-11 19:15:13 -0700

    added subclass for liquid, solid and gas material

--- pyrk/materials/material.py
+++ pyrk/materials/material.py
@@ -7,10 +7,10 @@ class Material(object):
     """This class represents a material. Its attributes are material properties
     and behaviors."""
 
-    def __init__(self, name=None,
+    def __init__(self,
+                 name=None,
                  k=0*units.watt/units.meter/units.kelvin,
                  cp=0*units.joule/units.kg/units.kelvin,
-                 mu=0*units.pascal*units.seconds,
                  dm=DensityModel()):
         """Initalizes a material
 
@@ -20,8 +20,6 @@ class Material(object):
         :type k: float, pint.unit.Quantity :math:'watt/meter/K'
         :param cp: specific heat capacity, :math:`c_p`, in :math:`J/kg-K`
         :type cp: float, pint.unit.Quantity :math:`J/kg-K`
-        :param mu: dynamic viscosity(for fluid), :math:`mu`, in :math:`Pa.s`
-        :type mu: float, pint.unit.Quantity :math:`Pa.s`
         :param dm: The density of the material
         :type dm: DensityModel object
         """
@@ -30,8 +28,6 @@ class Material(object):
         validation.validate_ge("k", k, 0*units.watt/units.meter/units.kelvin)
         self.cp = cp.to('joule/kg/kelvin')
         validation.validate_ge("cp", cp, 0*units.joule/units.kg/units.kelvin)
-        self.mu = mu.to('pascal*seconds')
-        validation.validate_ge("mu", mu, 0*units.pascal*units.seconds)
         self.dm = dm
 
     def rho(self, temp):
@@ -45,3 +41,77 @@ class Material(object):
         """
         ret = self.dm.rho(temp)
         return ret
+
+
+class LiquidMaterial(Material):
+    ''' subclass of material for liquid'''
+    def __init__(self,
+                 name=None,
+                 k=0*units.watt/units.meter/units.kelvin,
+                 cp=0*units.joule/units.kg/units.kelvin,
+                 dm=DensityModel(),
+                 mu=0*units.pascal*units.seconds):
+        """Initalizes a material
+
+        :param name: The name of the component (i.e., "fuel" or "cool")
+        :type name: str.
+        :param k: The thermal conductivity of the component
+        :type k: float, pint.unit.Quantity :math:'watt/meter/K'
+        :param cp: specific heat capacity, :math:`c_p`, in :math:`J/kg-K`
+        :type cp: float, pint.unit.Quantity :math:`J/kg-K`
+        :param dm: The density of the material
+        :type dm: DensityModel object
+        :param mu: dynamic viscosity(for fluid), :math:`mu`, in :math:`Pa.s`
+        :type mu: float, pint.unit.Quantity :math:`Pa.s`
+        """
+        Material.__init__(self, name, k, cp, dm)
+        self.mu = mu.to('pascal*seconds')
+        validation.validate_ge("mu", mu, 0*units.pascal*units.seconds)
+
+
+class SolidMaterial(Material):
+    ''' subclass of material for fluid'''
+    def __init__(self,
+                 name=None,
+                 k=0*units.watt/units.meter/units.kelvin,
+                 cp=0*units.joule/units.kg/units.kelvin,
+                 dm=DensityModel()):
+        """Initalizes a material
+
+        :param name: The name of the component (i.e., "fuel" or "cool")
+        :type name: str.
+        :param k: The thermal conductivity of the component
+        :type k: float, pint.unit.Quantity :math:'watt/meter/K'
+        :param cp: specific heat capacity, :math:`c_p`, in :math:`J/kg-K`
+        :type cp: float, pint.unit.Quantity :math:`J/kg-K`
+        :param dm: The density of the material
+        :type dm: DensityModel object
+        """
+        Material.__init__(self, name, k, cp, dm)
+
+
+class GasMaterial(Material):
+    ''' subclass of material for liquid'''
+    def __init__(self,
+                 name=None,
+                 k=0*units.watt/units.meter/units.kelvin,
+                 cp=0*units.joule/units.kg/units.kelvin,
+                 dm=DensityModel(),
+                 mu=0*units.pascal*units.seconds):
+        """Initalizes a material
+
+        :param name: The name of the component (i.e., "fuel" or "cool")
+        :type name: str.
+        :param k: The thermal conductivity of the component
+        :type k: float, pint.unit.Quantity :math:'watt/meter/K'
+        :param cp: specific heat capacity, :math:`c_p`, in :math:`J/kg-K`
+        :type cp: float, pint.unit.Quantity :math:`J/kg-K`
+        :param dm: The density of the material
+        :type dm: DensityModel object
+        :param mu: dynamic viscosity(for fluid), :math:`mu`, in :math:`Pa.s`
+        :type mu: float, pint.unit.Quantity :math:`Pa.s`
+        """
+        Material.__init__(self, name, k, cp, dm)
+        self.mu = mu.to('pascal*seconds')
+        validation.validate_ge("mu", mu, 0*units.pascal*units.seconds)
+
