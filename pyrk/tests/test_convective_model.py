6aae0c985baa746352c5f25a1ae27af8d1154881
Xin Wang <imwangxin@gmail.com>
2016-05-25 10:07:59 -0400

    corrected a test

--- pyrk/tests/test_convective_model.py
+++ pyrk/tests/test_convective_model.py
@@ -2,7 +2,7 @@ from nose.tools import assert_equal, assert_almost_equal, assert_true, \
     assert_false, assert_raises, assert_is_instance, with_setup
 from utilities.ur import units
 from convective_model import ConvectiveModel
-from materials.material import Material, LiquidMaterial
+from materials.liquid_material import LiquidMaterial
 
 def test_constant_model():
     h_constant = ConvectiveModel(
