7e4d4a744eb7e7e4b335527c974b60566ca3b8d6
andrewryh <andrewryh@gmail.com>
2017-02-25 16:48:35 -0600

    adds fix issue #40. Now works with both Python 2.7 and 3

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import io
 import os
 
 import pyrk
-#from version import get_git_version
+from pyrk.ver import get_git_version
 
 here = os.path.abspath(os.path.dirname(__file__))
 
@@ -22,7 +22,7 @@ long_description = read('README.md', 'CONTRIBUTING.md')
 
 setup(
     name='pyrk',
-    #version=get_git_version(),
+    version=get_git_version(),
     url='http://github.com/katyhuff/pyrk/',
     license='BSD 3-Clause License',
     author='Kathryn D. Huff',
